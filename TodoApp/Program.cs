751653c8e959843bab9c09e4aacb9005a7994ddc
shayansalehi617 <shayansalehi617@gmail.com>
2025-04-19 15:33:56 +0330

    EditIndexView

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -29,14 +29,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 }
 else
 {
